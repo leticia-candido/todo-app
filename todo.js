a27002a3534461328ab8ba8e40b4c8cd567c9877
Leticia Candido <leticia@MacBook-Pro-de-Leticia.local>
2020-10-07 17:38:40 -0300

    first commit

---
+++ todo.js
@@ -0,0 +1,75 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+
+
+/*var todos = [ //array
+    'Fazer café ',
+    'Estudar JavaScript ',
+    'Acessar comunidade da Rocketseat '
+];*/
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+
+function renderTodos() {
+    listElement.innerHTML = '';
+    
+
+
+    for ( todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#');
+    
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo('+ pos +')');
+
+
+        var linkText = document.createTextNode(' Excluir');
+
+    
+        
+    
+
+        linkElement.appendChild(linkText);
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
+
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos(); //chama a funçao pra fazer ela funcionar
+
+
+function addTodo(){
+    var todoText = inputElement.value;
+
+    todos.push(todoText); //push é para adicionar itens no array
+    inputElement.value = '';
+    renderTodos();
+    saveToStorage();
+
+} 
+
+buttonElement.onclick = addTodo;
+
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);//splice remove quantidade de itens do array, baseado pela posiçao
+    renderTodos();
+    saveToStorage();
+}
+
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
